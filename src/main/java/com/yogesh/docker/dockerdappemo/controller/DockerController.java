66ad64140d78f998919ea53f9197b1f4edd034ce
yogeshkadam <yogesh.kadam@aliz.ai>
2020-02-21 00:42:17 +0530

    Update code for dockerdemoapp

---
+++ src/main/java/com/yogesh/docker/dockerdappemo/controller/DockerController.java
@@ -0,0 +1,14 @@
+package com.yogesh.docker.dockerappdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+	@GetMapping("/message")
+	public String getMyName() {
+		
+		return "Simple Spring Boot Application";
+	}
+}
